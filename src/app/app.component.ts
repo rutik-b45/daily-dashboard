164ca6da9bc061963bbdfdb3d60afc9ae713c58b
Rutik Bhosale <rutik.bhosale@nonstopio.com>
2025-05-08 15:56:40 +0530

    setup routing for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'daily-dashboard';
+  currentTitle = 'Dashboard';
+
+  titleMap: { [key: string]: string } = {
+    '/': 'Dashboard',
+    '/weather': 'Weather',
+    '/news': 'News',
+    '/notes': 'Notes',
+    '/saved': 'Saved',
+  };
+
+  constructor(private router: Router) {
+    
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const url = event.urlAfterRedirects;
+        this.currentTitle = this.titleMap[url] || 'Dashboard';
+      });
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { NewsComponent } from './pages/news/news.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { SavedComponent } from './pages/saved/saved.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },             
+    { path: 'weather', component: WeatherComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'notes', component: NotesComponent },
+    { path: 'saved', component: SavedComponent },
+    { path: '**', redirectTo: '' }                          
+  ];
